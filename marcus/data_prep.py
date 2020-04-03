86ca4aedffcfa34a99d953e7f892d7f30f017b91
jylmarcus <jylm@users.noreply.github.com>
2020-04-03 16:01:48 +0800

    add data_prep to folder

---
+++ marcus/data_prep.py
@@ -0,0 +1,36 @@
+"""
+Should take in csv from a file source 
+and return pd.DataFrame, with its lagged values
+"""
+
+import pandas as pd
+
+## returns a csv with lagged values
+def get_lagged(filepath:str, index_col="Date", col2lag="Adj Close" , n=1) -> pd.DataFrame:
+    temp_df = pd.read_csv(filepath, index_col=index_col)
+    # lag the values
+    for lag in range(n):
+        temp_c_name = f"{col2lag}_{lag+1}"
+        temp_df[temp_c_name] = temp_df[col2lag].shift(lag+1)
+    
+    ## remove NA values
+    temp_df.dropna()
+    return temp_df
+
+## gets the % return of a columns
+def get_perc_return(df_filepath, column_name, n=1) -> pd.DataFrame:
+    '''
+    Column has to be the name of a column in the csv file
+    REturns data and result columns
+    '''
+    temp_df = pd.read_csv(df_filepath)
+    target = temp_df[column_name]
+    returns = (target - target.shift(n))/target.shift(n)
+    temp_df['%_returns'] = returns 
+    return temp_df[['Date', '%_returns']]
+
+if __name__ == '__main__':
+    get_lagged("./data/PPH_pharm_etf.csv", n=2)
+    results = get_perc_return("./data/SnP_500.csv", column_name="Open").head()
+    print(type(results))
+    print(results)
