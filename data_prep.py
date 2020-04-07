f24006dbc66e08653e42429b86d659bb1c14dae1
Ivanlxw <leexianwei96@gmail.com>
2020-04-07 14:24:59 +0800

    working ARMA+ADL

--- data_prep.py
+++ data_prep.py
@@ -7,6 +7,9 @@ import pandas as pd
 
 ## returns a csv with lagged values
 def get_lagged(filepath:str, index_col="Date", col2lag="Adj Close" , n=1) -> pd.DataFrame:
+    '''
+    check for filepath then check for pd.DataFrame in df
+    '''
     temp_df = pd.read_csv(filepath, index_col=index_col)
     # lag the values
     for lag in range(n):
@@ -14,8 +17,24 @@ def get_lagged(filepath:str, index_col="Date", col2lag="Adj Close" , n=1) -> pd.
         temp_df[temp_c_name] = temp_df[col2lag].shift(lag+1)
     
     ## remove NA values
-    temp_df.dropna()
+    temp_df = temp_df.dropna()
     return temp_df
+
+def get_lagged_df(df, index_col="Date", col2lag=["Open_SNP500"] , n=1):
+    '''
+    col2lag - list (MUST). list items must be in df's columns
+    '''
+    temp_df = df
+    # lag the values
+    for collag in col2lag:
+        for lag in range(n):
+            temp_c_name = f"{collag}_L{lag+1}"
+            temp_df[temp_c_name] = temp_df[collag].shift(lag+1)
+    
+    ## remove NA values
+    temp_df = temp_df.dropna()
+    return temp_df
+
 
 ## gets the % return of a columns
 def get_perc_return(df_filepath, column_name, n=1) -> pd.DataFrame:
@@ -31,6 +50,7 @@ def get_perc_return(df_filepath, column_name, n=1) -> pd.DataFrame:
 
 if __name__ == '__main__':
     get_lagged("./data/PPH_pharm_etf.csv", n=2)
-    results = get_perc_return("./data/SnP_500.csv", column_name="Open").head()
-    print(type(results))
-    print(results)
+    df = pd.read_csv("./data/output.csv")
+
+    final_df = get_lagged_df(df, col2lag=["Open_Pharm", "Volume_Pharm"],n=3).head()
+    print(final_df)
